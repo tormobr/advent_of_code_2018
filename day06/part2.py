2c11af8649d3d3be3bc637f1704c03068025e28e
tormobr <tormod.brandshoi@gmail.com>
2020-01-06 16:46:48 +0100

    LOC gets down

--- day06/part2.py
+++ day06/part2.py
@@ -1,34 +1,16 @@
 import numpy as np
 
 def part2(data):
-    grid_size = get_max(data)
+    grid_size = max(max(data, key=lambda x: x[0])[0],max(data, key=lambda x: x[1])[1])
     grid = create_grid(data, grid_size)
-    for y in range(grid_size):
-        for x in range(grid_size):
-            get_distance(x,y,data, grid)
-    grid_1d = np.array(grid).reshape((grid_size**2))
-    res = np.count_nonzero(grid_1d==2)
-    return res
+    [get_distance(x,y,data,grid) for y in range(grid_size) for x in range(grid_size)]
+    return np.count_nonzero(np.array(grid).reshape((grid_size**2))==2)
 
 def create_grid(data, n):
-    grid = []
-    for y in range(n):
-        grid.append([])
-        for x in range(n):
-            if (x, y) in data:
-                grid[y].append(1)
-            else:
-                grid[y].append(0)
-    return grid
-    
+    return [[1 if (x,y) in data else 0 for x in range(n)] for y in range(n)]
 
 def get_distance(x,y, data, grid):
     grid[y][x] = 2 if sum([abs(x1-x)+abs(y1-y) for (x1, y1) in data]) < 10000 else 0
-
-def get_max(data):
-    max_x = max(data, key=lambda x: x[0])[0]
-    max_y = max(data, key=lambda x: x[1])[1]
-    return max(max_x, max_y) +1
 
 if __name__ == "__main__":
     data = [tuple(map(int, line.strip().split(","))) for line in open("input.txt", "r")]
