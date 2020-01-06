50a3b475928ed878621bb0fd6085b5220667bcdb
tormobr <tormod.brandshoi@gmail.com>
2020-01-06 16:36:48 +0100

    part 2 cleaning#

--- day06/part2.py
+++ day06/part2.py
@@ -1,8 +1,4 @@
-import time
-import nltk
-from nltk import FreqDist
 import numpy as np
-from collections import defaultdict
 
 def part2(data):
     grid_size = get_max(data)
@@ -10,21 +6,19 @@ def part2(data):
     for y in range(grid_size):
         for x in range(grid_size):
             get_distance(x,y,data, grid)
-    fd = FreqDist(np.array(grid).reshape((grid_size**2)))
-    return fd[2]
+    grid_1d = np.array(grid).reshape((grid_size**2))
+    res = np.count_nonzero(grid_1d==2)
+    return res
 
 def create_grid(data, n):
     grid = []
-    index = 0 
     for y in range(n):
-        line = []
+        grid.append([])
         for x in range(n):
             if (x, y) in data:
-                line.append(1)
-                index += 1
+                grid[y].append(1)
             else:
-                line.append(0)
-        grid.append(line)
+                grid[y].append(0)
     return grid
     
 
@@ -35,15 +29,6 @@ def get_max(data):
     max_x = max(data, key=lambda x: x[0])[0]
     max_y = max(data, key=lambda x: x[1])[1]
     return max(max_x, max_y) +1
-
-def infinite(s, res, grid):
-    top = [grid[0][i] for i in range(len(grid[0]))]
-    bottom = [grid[-1][i] for i in range(len(grid[0]))]
-    left = [grid[i][0] for i in range(len(grid))]
-    right = [grid[i][-1] for i in range(len(grid))]
-    if s not in top+bottom+left+right:
-        return False
-    return True
 
 if __name__ == "__main__":
     data = [tuple(map(int, line.strip().split(","))) for line in open("input.txt", "r")]
